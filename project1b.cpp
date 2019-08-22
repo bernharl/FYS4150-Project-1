858aec8c76ca77d7e4eea82485f0b9e0a82396f3
Håkon Tansem <hakontan@gmail.com>
2019-08-22 17:57:01 +0200

    Created all arrays and matrices

--- project1b.cpp
+++ project1b.cpp
@@ -24,6 +24,19 @@ int main(int argc, char *argv[])
     double a = -1; 
     double b = 2; 
     double c = -1;
+    double h = 1.0/((double)n + 1);
+    double h_sq = h*h;
+    double v_arr[n];
+    double b_arr[n];
+    double x_arr[n];
+
+    for (int i=1; i<=n; i++)
+    {
+        x_arr[i] = i*h;
+        v_arr[i] = u(x_arr[i]);
+        b_arr[i] = f(x_arr[i]) * h_sq;
+        //cout << x_arr[i] <<endl;
+    }
 
     for (int i = 0; i <= n-1; i++){
         for (int j = 0; j<= n-1; j++){
@@ -31,7 +44,7 @@ int main(int argc, char *argv[])
             else if (i == (j + 1)) {A[i][j] = c;}
             else if (i == j - 1) {A[i][j] = a;}
             else {A[i][j] = 0;}
-            cout << A[i][j] << "|";
+            cout << A[i][j] << " ";
         }
         cout << endl;
     }
