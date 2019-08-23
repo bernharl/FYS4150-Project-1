be85eac31e48303cdce29836e0e9a9f180dad424
Nils-Ole Stutzer <nilsole2009@live.no>
2019-08-23 12:48:07 +0200

    Roughly finished row reduction algo.

--- project1b.cpp
+++ project1b.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
-#include "armadillo"
 
 using namespace std;
 
@@ -21,30 +20,48 @@ int main(int argc, char *argv[])
 
     filename = argv[1];
     n = atoi(argv[2]);
-    double A[n][n];
     double a = -1; 
     double b = 2; 
     double c = -1;
-    double h = 1.0/((double)n + 1);
+    double a_A[n-1];
+    double b_A[n];
+    double c_A[n-1];
+    double h = 1.0/((double) n + 1);
     double h_sq = h*h;
     double v_arr[n];
-    double b_arr[n];
+    double f_arr[n] = {1, 2, 3, 4, 5};
     double x_arr[n];
 
-    for (int i=1; i<=n; i++)
+    for (int i = 0; i < n; i++)
     {
         x_arr[i] = i*h;
         v_arr[i] = u(x_arr[i]);
-        b_arr[i] = f(x_arr[i]) * h_sq;
+        //f_arr[i] = f(x_arr[i]) * h_sq;
         b_A[i] = b;
-        //cout << x_arr[i] <<endl;
+        cout << f_arr[i] <<endl;
     }
-    for (int i=1; i<=n-1; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         a_A[i] = a;
         c_A[i] = c;
+        
     }
-    
+    for (int i = 1; i <= n; i++){
+        b_A[i] = b_A[i] - a_A[i-1] / b_A[i-1] * c_A[i-1];
+        f_arr[i] = f_arr[i] - a_A[i-1] / b_A[i-1] * f_arr[i-1];
+    }    
 
+    for (int i = n - 1; i >= 0; i--){
+        f_arr[i] = f_arr[i] - c_A[i] / b_A[i+1] * f_arr[i+1];
+        cout << "yo" << endl;
+    }
+
+    for (int i = 0; i < n; i++){
+        f_arr[i] /= b_A[i];
+    }
+
+    for (int i = 0; i < n; i++){
+        cout << f_arr[i]  << endl;
+    }
     return 0;
 }
