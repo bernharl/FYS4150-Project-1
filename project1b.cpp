a22503cac421b420a595f20343622c745238048b
Nils-Ole Stutzer <nilsole2009@live.no>
2019-08-23 13:43:12 +0200

    Debug

--- project1b.cpp
+++ project1b.cpp
@@ -20,48 +20,56 @@ int main(int argc, char *argv[])
 
     filename = argv[1];
     n = atoi(argv[2]);
+    cout << sizeof(double)*3*n/1000000.0 << endl;
+
     double a = -1; 
     double b = 2; 
     double c = -1;
     double a_A[n-1];
     double b_A[n];
     double c_A[n-1];
-    double h = 1.0/((double) n + 1);
+    double h = 1.0/((double) n + 1.0);
     double h_sq = h*h;
-    double v_arr[n];
+    double u_arr[n];
     double f_arr[n];
     double x_arr[n];
 
     for (int i = 0; i < n; i++)
     {
         x_arr[i] = i*h;
-        v_arr[i] = u(x_arr[i]);
+        u_arr[i] = u(x_arr[i]);
         f_arr[i] = f(x_arr[i]) * h_sq;
         b_A[i] = b;
         cout << "Original " << f_arr[i] <<endl;
     }
-    for (int i = 0; i < n - 1; i++)
-    {
+    int counter = 0;
+    for (int i = 0; i < n - 1; i++){
         a_A[i] = a;
         c_A[i] = c;
         
     }
-    for (int i = 1; i <= n; i++){
+    for (int i = 1; i < n; i++){
         b_A[i] = b_A[i] - a_A[i-1] / b_A[i-1] * c_A[i-1];
         f_arr[i] = f_arr[i] - a_A[i-1] / b_A[i-1] * f_arr[i-1];
+    
     }    
 
     for (int i = n - 1; i >= 0; i--){
-        f_arr[i] = f_arr[i] - c_A[i] / b_A[i+1] * f_arr[i+1];
+        counter += 1;
+
+        f_arr[i] = f_arr[i] - c_A[i] / b_A[i] * f_arr[i+1];
+        cout << f_arr[i]  << " i: " << i << endl;
+
     }
+    cout << "counter " << counter << endl;
 
     for (int i = 0; i < n; i++){
         f_arr[i] /= b_A[i];
+        
     }
 
-    for (int i = 0; i < n; i++){
-        cout << f_arr[i]  << endl;
-    }
+    //for (int i = 0; i < n; i++){
+    //}
     
     return 0;
 }
