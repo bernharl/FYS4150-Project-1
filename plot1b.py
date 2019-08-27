f4345cafb8e60b0fe15ad2cc593efd631ad721b2
bernharl <bernharl@student.matnat.uio.no>
2019-08-27 10:58:50 +0200

    Restored accidentally deleted plot script, some cleanup of main cpp code

---
+++ plot1b.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt("data.txt", skiprows = 1)
+v = data[:, 0]
+u = data[:, 1]
+x = data[:, 2]
+error = data[:, 3]
+
+plt.plot(x, v, label = "Numerical")
+plt.plot(x, u, label = "Analytical")
+plt.legend()
+plt.show()
--- project1b.cpp
+++ project1b.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <ctime>
 
+
 using namespace std;
 
 inline double f(double x)
@@ -16,8 +17,8 @@ inline double u(double x)
 
 void Thomas_algorithm(string filename, int n)
 {
-    double a = -1.0; 
-    double b = 2.0; 
+    double a = -1.0;
+    double b = 2.0;
     double c = -1.0;
     double* a_A = new double[n-1];
 
@@ -43,7 +44,7 @@ void Thomas_algorithm(string filename, int n)
     for (int i = 0; i < n - 1; i++) {
         a_A[i] = a;
         c_A[i] = c;
-        
+
     }
     clock_t t_start = clock();
     for (int i = 1; i <= n; i++) {
@@ -51,8 +52,8 @@ void Thomas_algorithm(string filename, int n)
         //n - 1 iterations
         b_A[i] = b_A[i] - a_A[i-1] / b_A[i-1] * c_A[i-1];
         f_arr[i] = f_arr[i] - a_A[i-1] / b_A[i-1] * f_arr[i-1];
-    
-    }    
+
+    }
 
     for (int i = n - 1; i >= 0; i--) {
         //3 floating point operations
@@ -67,49 +68,48 @@ void Thomas_algorithm(string filename, int n)
         //1 floating point operation
         //n-1 iterations
         f_arr[i] /= b_A[i];
-        
+
     }
     clock_t t_end = clock();
     double CPU_time = 1000.0 * (t_end - t_start) / CLOCKS_PER_SEC;
     cout << "CPU time: " << CPU_time << " ms " << endl;
-    /* 
+    /*
     ofstream outfile;
     outfile.open(filename);
-    
+
     outfile << " v_i " << " u " << " x " << " Error " << endl;
     cout << u_arr[0] << endl;
     for (int i = 1; i < n; i++) {
         double error = abs((u_arr[i] - f_arr[i])/u_arr[i]);
         outfile << f_arr[i] << " " << u_arr[i] << " " << x_arr[i] << " "<< error << endl;
     }
-    outfile.close(); 
+    outfile.close();
     */
     int operations;
     operations = 6*(n - 1) + 3*(n - 1) + (n - 1);
     cout << operations << endl;
 
     delete [] a_A; delete [] b_A; delete [] c_A;
-    delete [] u_arr; delete [] f_arr; delete [] x_arr; 
-    
+    delete [] u_arr; delete [] f_arr; delete [] x_arr;
+
 }
 void Specialized_algorithm(string filename, int n)
 {
-    double a = -1.0; 
-    double b = 2.0; 
+    double a = -1.0;
+    double b = 2.0;
     double c = -1.0;
 
     double h = 1.0/((double) n + 1.0);
-    double h_sq = h*h;
+    double h_sq = h * h;
     double* u_arr = new double[n];
     double* f_arr = new double[n];
     double* x_arr = new double[n];
 
     for (int i = 0; i < n; i++)
     {
-        x_arr[i] = i*h;
+        x_arr[i] = i * h;
         u_arr[i] = u(x_arr[i]);
         f_arr[i] = f(x_arr[i]) * h_sq;
-        b_A[i] = b;
         //cout << "Original " << f_arr[i] <<endl;
     }
     int counter = 0;
@@ -119,49 +119,53 @@ void Specialized_algorithm(string filename, int n)
     //algoritme
     double A = a - pow(b, 2)/a;
     double ba = b/a;
-    double bA = b/A
+    double bA = b/A;
 
     for (int i=1; i<=n; i++)
     {
-        f_arr[i] = f_arr[i] - ba*f[i-1]
+        f_arr[i] = f_arr[i] - ba * f_arr[i-1];
     }
     for (int i=n-1; i>=0; i--)
     {
-        f_arr[i] = f_arr[i] - bA*f[i+1]
+        f_arr[i] = f_arr[i] - bA * f_arr[i+1];
     }
     for (int i = 0; i < n; i++) {
         //1 floating point operation
         //n-1 iterations
-        f_arr[i] /= b_A[i];    
+        //f_arr[i] /= b_A[i];
+        f_arr[i] /= b;
     }
-    
-    /* 
+
+    /*
     ofstream outfile;
     outfile.open(filename);
-    
+
     outfile << " v_i " << " u " << " x " << " Error " << endl;
     cout << u_arr[0] << endl;
     for (int i = 1; i < n; i++) {
         double error = abs((u_arr[i] - f_arr[i])/u_arr[i]);
         outfile << f_arr[i] << " " << u_arr[i] << " " << x_arr[i] << " "<< error << endl;
     }
-    outfile.close(); 
+    outfile.close();
     */
+    clock_t t_end = clock();
+    double CPU_time = 1000.0 * (t_end - t_start) / CLOCKS_PER_SEC;
+    cout << "CPU time: " << CPU_time << " ms " << endl;
+    //delete [] a_A; delete [] b_A; delete [] c_A;
+    delete [] u_arr; delete [] f_arr; delete [] x_arr;
 
-    delete [] a_A; delete [] b_A; delete [] c_A;
-    delete [] u_arr; delete [] f_arr; delete [] x_arr; 
-   
 }
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    
+
     int n;
     string filename;
 
     filename = argv[1];
     n = atoi(argv[2]);
-    
+
     Thomas_algorithm(filename, n);
+    Specialized_algorithm(filename, n);
 
     return 0;
 }
