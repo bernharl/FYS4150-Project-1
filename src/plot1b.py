5bdfb44844c5d4b51634cdb4ed2330bc5745820b
Håkon Tansem <hakontan@gmail.com>
2019-08-29 12:52:06 +0200

    Added extra if statements to plot module

--- src/plot1b.py
+++ src/plot1b.py
@@ -1,7 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
-data = np.loadtxt("data_thomas.txt", skiprows = 1)
+if len(sys.argv) > 2:
+    print("Only one data file can be plotted.\n Please enter only one data file in command line")
+    sys.exit(1)
+
+if len(sys.argv) == 2:
+    datafile = sys.argv[1]
+
+if len(sys.argv) < 2:
+    datafile = input("Please enter the name of the data folder you wish to plot: ")
+
+
+data = np.loadtxt(datafile, skiprows = 1)
 v = data[:, 0]
 u = data[:, 1]
 x = data[:, 2]
