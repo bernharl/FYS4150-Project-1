b292b12d30d12e7f853b939f7d8ca6d10c96b892
SagittariusA-Star <nilsole2009@live.no>
2019-08-29 14:13:12 +0200

    Trying to obtain Bernards fix

--- src/plot1b.py
+++ src/plot1b.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("data_thomas.txt", skiprows = 1)
+data = np.loadtxt("data.dat", skiprows = 1)
 v = data[:, 0]
 u = data[:, 1]
 x = data[:, 2]
 error = data[:, 3]
 
 plt.plot(x, v, label = "Numerical")
-plt.plot(x, u, label = "Analytical")
+plt.plot(x, u, "r--", label = "Analytical")
 plt.legend()
 plt.show()
--- src/project1b.cpp
+++ src/project1b.cpp
@@ -51,6 +51,7 @@ void Thomas_algorithm(string filename, int n)
         //n - 1 iterations
         b_A[i] = b_A[i] - a_A[i-1] / b_A[i-1] * c_A[i-1];
         f_arr[i] = f_arr[i] - a_A[i-1] / b_A[i-1] * f_arr[i-1];
+        cout << b_A[i] << endl;
 
     }
 
@@ -106,6 +107,7 @@ void Specialized_algorithm(string filename, int n)
     double* u_arr = new double[n];
     double* f_arr = new double[n];
     double* x_arr = new double[n];
+    double* b_tilde = new double[n];
 
     for (int i = 0; i < n; i++)
     {
@@ -113,29 +115,29 @@ void Specialized_algorithm(string filename, int n)
         u_arr[i] = u(x_arr[i]);
         f_arr[i] = f(x_arr[i]) * h_sq;
         //cout << "Original " << f_arr[i] <<endl;
+        b_tilde[i] = 2.0 - i / (i + 1.0);
+        cout << b_tilde[i] << "  " << i << endl;
+
     }
+
+
     int counter = 0;
 
 
     clock_t t_start = clock();
     //algoritme
-    double B = b - pow(a, 2) / b;
-    //cout << A << endl;
-    double ab = a / b;
-    //cout << ba << endl;
-    double aB = a / B;
-    //cout << bA << endl;
+
 
     for (int i=1; i<=n; i++)
     {
         //f_arr[i] = f_arr[i] - c_A[i] / b_A[i] * f_arr[i+1];
-        f_arr[i] = f_arr[i] - ab * f_arr[i-1];
+        f_arr[i] = f_arr[i] + 1.0 / b_tilde[i - 1] * f_arr[i-1];
         //cout << f_arr[i] << endl;
     }
 
     for (int i=n-1; i>=0; i--)
     {
-        f_arr[i] = f_arr[i] - aB * f_arr[i+1];
+        f_arr[i] = f_arr[i] + 1.0 / b_tilde[i+1] * f_arr[i+1];
         //cout << f_arr[i] << endl;
     }
 
@@ -145,9 +147,6 @@ void Specialized_algorithm(string filename, int n)
         //f_arr[i] /= b_A[i];
         f_arr[i] /= b;
     }
-
-
-
 
     ofstream outfile;
     outfile.open(filename);
@@ -167,7 +166,7 @@ void Specialized_algorithm(string filename, int n)
     clock_t t_end = clock();
     double CPU_time = 1000.0 * (t_end - t_start) / CLOCKS_PER_SEC;
     cout << "CPU time: " << CPU_time << " ms " << endl;
-    delete [] u_arr; delete [] f_arr; delete [] x_arr;
+    delete [] u_arr; delete [] f_arr; delete [] x_arr; delete[] b_tilde;
   
 }
 int main(int argc, char *argv[])
