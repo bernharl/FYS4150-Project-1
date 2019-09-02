0277982719820e7aa5b011143edc3395524fa602
Nils-Ole Stutzer <nilsole2009@live.no>
2019-09-02 09:56:15 +0200

    Added more to method section, and made a header file with keyword args

---
+++ src/keyword_args.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int n = 1e2;
+int exponent = 7;
+string data_name = "data.dat";
+string error_name = "error.dat";
+string CPU_time_name = "CPU_time.dat";
