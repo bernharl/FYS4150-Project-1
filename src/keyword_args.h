3f0717ee4912563162d3178ca9e25a07f27563ec
Nils-Ole Stutzer <nilsole2009@live.no>
2019-09-05 16:44:13 +0200

    General clean up and commenting. Changed back exponent step to 0.1 but with round(pow(10, i))

--- src/keyword_args.h
+++ src/keyword_args.h
@@ -2,11 +2,17 @@
 #include <string>
 using namespace std;
 
-int n = 1e2;
-int exponent = 7;
-string data_name = "data.dat";
-string thomas_name = "thomas.dat";
-string special_name = "special.dat";
-
+// Single grid size 
+int n = 1e2;        
+// Upper level of exponents to run over in max(error) vs n
+int exponent = 7;   
+// Name of data file used for plotting u[i] and v[i] vs x[i]
+string data_name = "data.dat";  
+// Name of thomas file to save max log10(error) and CPU time vs n
+string thomas_name = "thomas.dat";  
+// Name of specialised thomas file to save max log10(error) and CPU time vs n
+string special_name = "special.dat";    
+// Name of data file used for specialised Thomas algorithm 
 string data_name_special = "data_special.dat";
+// Name of data file used for Thomas algorithm 
 string data_name_thomas = "data_thomas.dat";
